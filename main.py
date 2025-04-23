0391b7429c5065570a602ee0b2ec5cf6be896b9b
Ramyashree517 <ramyashreebly@gmail.com>
2025-04-23 15:09:33 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,136 +1,156 @@
-from flask import Flask, request, render_template, jsonify  # Import jsonify
+# # importing the dependices
+
+# import numpy as np
+# import pandas as pd
+# from sklearn.preprocessing import StandardScaler
+# from sklearn.model_selection import train_test_split
+# from sklearn import svm
+# from sklearn.metrics import accuracy_score
+
+# # Data Collection and analysis
+# # PIMA Diabates Dataset
+# # loading the diabetes dataset to a pandas Dataframe
+
+# diabetes_dataset=pd.read_csv("diabetes.csv")
+# # pd.read_csv?
+# pd.read_csv
+
+# # printing the first 5 rows of the dataset
+# print(diabetes_dataset.head())
+
+# # number of rows and columns
+# diabetes_dataset.shape
+
+# # Getting the statistical mesaures of the data
+# diabetes_dataset.describe()
+# diabetes_dataset['Outcome'].value_counts()
+
+# diabetes_dataset.groupby('Outcome').mean()
+
+# #separting the data and labels
+# X=diabetes_dataset.drop(columns='Outcome',axis=1)
+# Y=diabetes_dataset['Outcome']
+# print(X)
+# print(Y)
+
+# # data standarized
+
+# scaler=StandardScaler()
+# scaler.fit(X)
+# standardized_data=scaler.transform(X)
+# print(standardized_data)
+
+# X=standardized_data
+# Y=diabetes_dataset['Outcome']
+# print(X)
+# print(Y)
+
+# # Train Test Spilt
+
+# X_train, X_test, Y_train, Y_test=train_test_split(X,Y, test_size=0.2,stratify=Y, random_state=2)
+# print(X.shape, X_train.shape, X_test.shape)
+
+# # training the model
+# classifier=svm.SVC(kernel='linear')
+
+# # training the support vector function classifier
+# classifier.fit(X_train, Y_train)
+
+# # Model Evalution
+# #  accuracy_score
+
+# #accuracy score on the training data
+# X_train_prediction =classifier.predict(X_train)
+# training_data_accuracy=accuracy_score(X_train_prediction, Y_train)
+# print("Accuracy score of the training data:",training_data_accuracy)
+
+# X_test_prediction =classifier.predict(X_test)
+# test_data_accuracy=accuracy_score(X_test_prediction, Y_test)
+# print("Accuracy score of the test data:",test_data_accuracy)
+
+# # making a predction system
+
+# input_data=(1,89,66,23,94,28.1,0.167,21)
+
+# #changeing the input data to numpy array
+# input_data_as_numpy_array=np.asarray(input_data)
+
+# #reshape the array as we are predicting for one instance
+# input_data_reshaped=input_data_as_numpy_array.reshape(1, -1)
+
+# #standarised the input data
+# std_data=scaler.transform(input_data_reshaped)
+# print(std_data)
+
+# prediction=classifier.predict(std_data)
+# print(prediction)
+
+# if(prediction[0]==0):
+#     print("the person is not diabetic")
+# else:
+#     print("the person is diabetic")
+
+
+from flask import Flask, render_template, request, jsonify
 import numpy as np
 import pandas as pd
-import pickle
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn import svm
 import psycopg2
-from psycopg2 import sql
-import os
-from dotenv import load_dotenv
+from psycopg2 import Error
 
-load_dotenv()  # Load environment variables from .env file
-
-# Database connection
-def get_db_connection():
-    return psycopg2.connect(os.getenv('DATABASE_URL'))
-
-
-# flask app
 app = Flask(__name__)
 
-# load databasedataset===================================
-sym_des = pd.read_csv("datasets/symtoms_df.csv")
-precautions = pd.read_csv("datasets/precautions_df.csv")
-workout = pd.read_csv("datasets/workout_df.csv")
-description = pd.read_csv("datasets/description.csv")
-medications = pd.read_csv('datasets/medications.csv')
-diets = pd.read_csv("datasets/diets.csv")
 
+# Neon.tech database connection string
+NEON_CONNECTION_STRING = "postgresql://neondb_owner_owner:npg_ocwhasli2V7d@ep-shy-meadow-a54ni1fm-pooler.us-east-2.aws.neon.tech/neondb_owner?sslmode=require"
 
-# load model===========================================
-svc = pickle.load(open('models/svc.pkl','rb'))
+# Load and train diabetes model
+diabetes_dataset = pd.read_csv("diabetes.csv")
+X = diabetes_dataset.drop(columns='Outcome', axis=1)
+Y = diabetes_dataset['Outcome']
 
+scaler = StandardScaler()
+scaler.fit(X)
+standardized_data = scaler.transform(X)
 
-#============================================================
-# custome and helping functions
-#==========================helper funtions================
-def helper(dis):
-    desc = description[description['Disease'] == dis]['Description']
-    desc = " ".join([w for w in desc])
+X_train, X_test, Y_train, Y_test = train_test_split(standardized_data, Y, test_size=0.2, stratify=Y, random_state=2)
 
-    pre = precautions[precautions['Disease'] == dis][['Precaution_1', 'Precaution_2', 'Precaution_3', 'Precaution_4']]
-    pre = [col for col in pre.values]
+classifier = svm.SVC(kernel='linear')
+classifier.fit(X_train, Y_train)
 
-    med = medications[medications['Disease'] == dis]['Medication']
-    med = [med for med in med.values]
-
-    die = diets[diets['Disease'] == dis]['Diet']
-    die = [die for die in die.values]
-
-    wrkout = workout[workout['disease'] == dis]['workout']
-
-    # If no precautions are found, handle it gracefully
-    if len(pre) == 0:
-        pre.append(['No precautions available'])
-
-    return desc, pre, med, die, wrkout
-
-symptoms_dict = {'itching': 0, 'skin_rash': 1, 'nodal_skin_eruptions': 2, 'continuous_sneezing': 3, 'shivering': 4, 'chills': 5, 'joint_pain': 6, 'stomach_pain': 7, 'acidity': 8, 'ulcers_on_tongue': 9, 'muscle_wasting': 10, 'vomiting': 11, 'burning_micturition': 12, 'spotting_ urination': 13, 'fatigue': 14, 'weight_gain': 15, 'anxiety': 16, 'cold_hands_and_feets': 17, 'mood_swings': 18, 'weight_loss': 19, 'restlessness': 20, 'lethargy': 21, 'patches_in_throat': 22, 'irregular_sugar_level': 23, 'cough': 24, 'high_fever': 25, 'sunken_eyes': 26, 'breathlessness': 27, 'sweating': 28, 'dehydration': 29, 'indigestion': 30, 'headache': 31, 'yellowish_skin': 32, 'dark_urine': 33, 'nausea': 34, 'loss_of_appetite': 35, 'pain_behind_the_eyes': 36, 'back_pain': 37, 'constipation': 38, 'abdominal_pain': 39, 'diarrhoea': 40, 'mild_fever': 41, 'yellow_urine': 42, 'yellowing_of_eyes': 43, 'acute_liver_failure': 44, 'fluid_overload': 45, 'swelling_of_stomach': 46, 'swelled_lymph_nodes': 47, 'malaise': 48, 'blurred_and_distorted_vision': 49, 'phlegm': 50, 'throat_irritation': 51, 'redness_of_eyes': 52, 'sinus_pressure': 53, 'runny_nose': 54, 'congestion': 55, 'chest_pain': 56, 'weakness_in_limbs': 57, 'fast_heart_rate': 58, 'pain_during_bowel_movements': 59, 'pain_in_anal_region': 60, 'bloody_stool': 61, 'irritation_in_anus': 62, 'neck_pain': 63, 'dizziness': 64, 'cramps': 65, 'bruising': 66, 'obesity': 67, 'swollen_legs': 68, 'swollen_blood_vessels': 69, 'puffy_face_and_eyes': 70, 'enlarged_thyroid': 71, 'brittle_nails': 72, 'swollen_extremeties': 73, 'excessive_hunger': 74, 'extra_marital_contacts': 75, 'drying_and_tingling_lips': 76, 'slurred_speech': 77, 'knee_pain': 78, 'hip_joint_pain': 79, 'muscle_weakness': 80, 'stiff_neck': 81, 'swelling_joints': 82, 'movement_stiffness': 83, 'spinning_movements': 84, 'loss_of_balance': 85, 'unsteadiness': 86, 'weakness_of_one_body_side': 87, 'loss_of_smell': 88, 'bladder_discomfort': 89, 'foul_smell_of urine': 90, 'continuous_feel_of_urine': 91, 'passage_of_gases': 92, 'internal_itching': 93, 'toxic_look_(typhos)': 94, 'depression': 95, 'irritability': 96, 'muscle_pain': 97, 'altered_sensorium': 98, 'red_spots_over_body': 99, 'belly_pain': 100, 'abnormal_menstruation': 101, 'dischromic _patches': 102, 'watering_from_eyes': 103, 'increased_appetite': 104, 'polyuria': 105, 'family_history': 106, 'mucoid_sputum': 107, 'rusty_sputum': 108, 'lack_of_concentration': 109, 'visual_disturbances': 110, 'receiving_blood_transfusion': 111, 'receiving_unsterile_injections': 112, 'coma': 113, 'stomach_bleeding': 114, 'distention_of_abdomen': 115, 'history_of_alcohol_consumption': 116, 'fluid_overload.1': 117, 'blood_in_sputum': 118, 'prominent_veins_on_calf': 119, 'palpitations': 120, 'painful_walking': 121, 'pus_filled_pimples': 122, 'blackheads': 123, 'scurring': 124, 'skin_peeling': 125, 'silver_like_dusting': 126, 'small_dents_in_nails': 127, 'inflammatory_nails': 128, 'blister': 129, 'red_sore_around_nose': 130, 'yellow_crust_ooze': 131, }
-diseases_list = {15: 'Fungal infection', 4: 'Allergy', 16: 'GERD', 9: 'Chronic cholestasis', 14: 'Drug Reaction', 33: 'Peptic ulcer diseae', 1: 'AIDS', 12: 'Diabetes ', 17: 'Gastroenteritis', 6: 'Bronchial Asthma', 23: 'Hypertension ', 30: 'Migraine', 7: 'Cervical spondylosis', 32: 'Paralysis (brain hemorrhage)', 28: 'Jaundice', 29: 'Malaria', 8: 'Chicken pox', 11: 'Dengue', 37: 'Typhoid', 40: 'hepatitis A', 19: 'Hepatitis B', 20: 'Hepatitis C', 21: 'Hepatitis D', 22: 'Hepatitis E', 3: 'Alcoholic hepatitis', 36: 'Tuberculosis', 10: 'Common Cold', 34: 'Pneumonia', 13: 'Dimorphic hemmorhoids(piles)', 18: 'Heart attack', 39: 'Varicose veins', 26: 'Hypothyroidism', 24: 'Hyperthyroidism', 25: 'Hypoglycemia', 31: 'Osteoarthristis', 5: 'Arthritis', 0: '(vertigo) Paroymsal  Positional Vertigo', 2: 'Acne', 38: 'Urinary tract infection', 35: 'Psoriasis', 27: 'Impetigo'}
-
-# Model Prediction function
-def get_predicted_value(patient_symptoms):
-    input_vector = np.zeros(len(symptoms_dict))
-    for item in patient_symptoms:
-        if item in symptoms_dict:
-            input_vector[symptoms_dict[item]] = 1
-        else:
-            print(f"Warning: '{item}' not found in symptoms_dict.")
-            return "Error: One or more symptoms not recognized."
-    return diseases_list[svc.predict([input_vector])[0]]
-
-
-
-
-
-
-# creating routes========================================
-
-
-@app.route("/")
-def index():
-    return render_template("index.html")
-
-# Define a route for the home page
-@app.route('/predict', methods=['GET', 'POST'])
+@app.route('/')
 def home():
-    if request.method == 'POST':
-        symptoms = request.form.get('symptoms')
-        print(symptoms)
-        if symptoms == "Symptoms":
-            message = "Please either write symptoms or you have written misspelled symptoms"
-            return render_template('index.html', message=message)
-        else:
-            user_symptoms = [s.strip() for s in symptoms.split(',')]
-            user_symptoms = [symptom.strip("[]' ") for symptom in user_symptoms]
-
-            predicted_disease = get_predicted_value(user_symptoms)
-            dis_des, precautions, medications, rec_diet, workout = helper(predicted_disease)
-
-            my_precautions = []
-            if len(precautions) > 0:  # Check if precautions list is not empty
-                for i in precautions[0]:
-                    my_precautions.append(i)
-            else:
-                my_precautions.append("No precautions available")
-
-            return render_template('index.html', predicted_disease=predicted_disease, dis_des=dis_des,
-                                   my_precautions=my_precautions, medications=medications, my_diet=rec_diet,
-                                   workout=workout)
-
     return render_template('index.html')
 
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        pregnancies = float(request.form['pregnancies'])
+        glucose = float(request.form['glucose'])
+        blood_pressure = float(request.form['blood_pressure'])
+        skin_thickness = float(request.form['skin_thickness'])
+        insulin = float(request.form['insulin'])
+        bmi = float(request.form['bmi'])
+        diabetes_pedigree_function = float(request.form['diabetes_pedigree_function'])
+        age = float(request.form['age'])
 
+        input_data = np.array([pregnancies, glucose, blood_pressure, skin_thickness, 
+                             insulin, bmi, diabetes_pedigree_function, age])
+        input_data = input_data.reshape(1, -1)
+        std_data = scaler.transform(input_data)
 
-# about view funtion and path
-@app.route('/about')
-def about():
-    return render_template("about.html")
-# contact view funtion and path
-@app.route('/contact')
-def contact():
-    return render_template("contact.html")
+        prediction = classifier.predict(std_data)
+        result = "The person is diabetic" if prediction[0] == 1 else "The person is not diabetic"
+        
+        return render_template('index.html', result=result)
+    
+    except Exception as e:
+        error_message = f"Error in prediction: {str(e)}"
+        return render_template('index.html', result=error_message)
 
-# developer view funtion and path
-@app.route('/developer')
-def developer():
-    return render_template("developer.html")
-
-# about view funtion and path
-@app.route('/blog')
-def blog():
-    return render_template("blog.html")
-
-@app.route('/book_appointment', methods=['POST'])
+@app.route('/book-appointment', methods=['POST'])
 def book_appointment():
     try:
         name = request.form['name']
@@ -138,23 +158,38 @@ def book_appointment():
         phone = request.form['phone']
         date = request.form['date']
         time = request.form['time']
-        comments = request.form['comments']
 
-        conn = get_db_connection()
-        cur = conn.cursor()
-        cur.execute(
-            sql.SQL("INSERT INTO appointments (name, email, phone, date, time, comments) VALUES (%s, %s, %s, %s, %s, %s)"),
-            (name, email, phone, date, time, comments)
-        )
-        conn.commit()
-        cur.close()
-        conn.close()
+        if not all([name, email, phone, date, time]):
+            return jsonify({'status': 'error', 'message': 'All fields are required'})
 
-        return jsonify({'success': True, 'message': 'Appointment booked successfully'})
-    except Exception as e:
-        return jsonify({'success': False, 'message': str(e)})
+        # Connect to Neon.tech database
+        connection = psycopg2.connect(NEON_CONNECTION_STRING)
+        cursor = connection.cursor()
 
+        insert_query = """
+            INSERT INTO appointments (name, email, phone, appointment_date, appointment_time)
+            VALUES (%s, %s, %s, %s, %s)
+        """
+        cursor.execute(insert_query, (name, email, phone, date, time))
+        
+        connection.commit()
+        
+        return jsonify({
+            'status': 'success',
+            'message': 'Appointment booked successfully!'
+        })
+
+    except Error as e:
+        return jsonify({
+            'status': 'error',
+            'message': f'Error booking appointment: {str(e)}'
+        })
+    
+    finally:
+        if 'cursor' in locals():
+            cursor.close()
+        if 'connection' in locals():
+            connection.close()
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(debug=True)
